f657ef153ab9399777bc8f288abc9d1410150a68
Kolupaeva Anna <131313anna84@gmail.com>
2017-06-12 22:48:18 +0300

    HW3

--- HW3/src/main.js
+++ HW3/src/main.js
@@ -2,8 +2,8 @@
 
 let arr = [1, 2, 3];
 function addToArray(arrParam, newElem) {
-	 arrParam[arrParam.length] = newElem;
-	 return arrParam;
+  arrParam[arrParam.length] = newElem;
+  return arrParam;
 }
 
 console.log(addToArray(arr, 4));
@@ -13,8 +13,7 @@ console.log(addToArray(arr, 4));
 
 var userNames = [{ name: 'Egor' }, { name: 'Katya' }, { name: 'Vera' }];
 function simpleObjectGenerator (param1, param2, param3) {
-
-return ({argument1: param1, argument2: param2, argument3: param3});
+  return ({argument1: param1, argument2: param2, argument3: param3});
 }
 
 console.log(simpleObjectGenerator('protocol', { url: '22' }, 8000));
@@ -25,11 +24,12 @@ console.log(simpleObjectGenerator(77, userNames, 'privet kak dela chto novogo'.t
 
 var myName = { name: 'Ivan' };
 function addNameToUser(newKey, newValue, userName) {
-	newObj = {};
-	newObj[newKey]=newValue;
-	for (name in userName)
-		newObj.name = userName.name;
-	return newObj;
+  let newObj = {};
+  newObj[newKey]=newValue;
+  if (userName.name !== undefined) {
+	newObj.name = userName.name;
+  }
+  return newObj;
 }
 console.log(addNameToUser('family', '%Ivanov%', myName));
 console.log(myName);
@@ -38,21 +38,21 @@ console.log(myName);
 //Task 4
 
 function fizzBuzz(num) {
-	if (num%5 == 0 && num%3 == 0) {
-	  	return ('FizzBuzz');
-	} else {
-	  	if (num%5 == 0) {
-	  		return ('Buzz');
-	  	} else {
-	  		if (num%3 == 0) {
-	  			return ('Fizz');
-	  		} else {
-	  			if (num%5 != 0 && num%3 != 0) {
-	  				return (num);
-	  			}
-	  		}
+  if (num%5 == 0 && num%3 == 0) {
+	return ('FizzBuzz');
+  } else {
+	if (num%5 == 0) {
+	  return ('Buzz');
+    } else {
+	  if (num%3 == 0) {
+	  return ('Fizz');
+	  } else {
+	  	if (num%5 != 0 && num%3 != 0) {
+	  	  return (num);
 	  	}
+	  }
 	}
+  }
 }
 console.log(fizzBuzz(7));
 
@@ -61,10 +61,10 @@ console.log(fizzBuzz(7));
 
 let array = [];
 function Func1(arg1, arg2, arg3, Func2) {
-	array[0] = arg1;
-	array[1] = arg2;
-	array[2] = arg3;
-	Func2(array);
+  array[0] = arg1;
+  array[1] = arg2;
+  array[2] = arg3;
+  Func2(array);
 }
 
 Func1( 1, 2, 3, function(f) {console.log (f)}); 
