358822bbcd4b0702f8a494c632e4d9082e956282
Kolupaeva Anna <131313anna84@gmail.com>
2017-06-12 22:12:47 +0300

    HW1

--- HW1/src/main.js
+++ HW1/src/main.js
@@ -1,19 +1,20 @@
 
+
 var	contact = {
-	number: 1,						// номер п/п
-	name: 'Иванов Иван',			// ФИО
-	phone: '777-77-77',				// номер телефона
-	status_online: true,			// в сети/не в сети
-	add_contact: {					// дополнительные контакты
-		e_mail: 'ivan@gmail.com',							// e-mail
-		add_phone: '000-777-77-77',							// дополнительный номер телефона
-		address: ['Харьков', 'ул. Сумская', 'д.1', 'кв. 1']	// адрес
-	}
+  number: 1,						// номер п/п
+  name: 'Иванов Иван',			// ФИО
+  phone: '777-77-77',				// номер телефона
+  status_online: true,			// в сети/не в сети
+  add_contact: {					// дополнительные контакты
+  	e_mail: 'ivan@gmail.com',							// e-mail
+  	add_phone: '000-777-77-77',							// дополнительный номер телефона
+  	address: ['Харьков', 'ул. Сумская', 'д.1', 'кв. 1']	// адрес
+  }
 };
 
 console.log(contact); 
 console.log('Object 2 - Дополнительные контакты: ', contact.add_contact); 
 console.log('Array - Адрес: ', contact.add_contact.address[0], 
-	contact.add_contact.address[1],
-	contact.add_contact.address[2],
-	contact.add_contact.address[3]);
+contact.add_contact.address[1],
+contact.add_contact.address[2],
+contact.add_contact.address[3]);
